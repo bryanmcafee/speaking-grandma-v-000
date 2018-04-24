1b51f38ec9a88e987e1340c1fabbfb40e1b4e99e
Bryan McAfee <brymcafee@gmail.com>
2018-04-24 15:03:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase !=phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
-  else
+  elsif phrase =phrase.upcase()
     return "NO, NOT SINCE 1938!"
 
     end
