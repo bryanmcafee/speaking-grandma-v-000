c4f08275d7744bca5b0b1ce98962c2b810d75a7b
Bryan McAfee <brymcafee@gmail.com>
2018-04-24 14:32:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase !=phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
-  elsif     
+  elsif
     return "NO, NOT SINCE 1938!"
-  else phrase == " ILOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
+
     end
 end
