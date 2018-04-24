7d82b57aee19973e501ca88669744647d92dc908
Bryan McAfee <brymcafee@gmail.com>
2018-04-24 14:23:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   elsif
     return "NO, NOT SINCE 1938!"
   else
-    if phrase == ("I LOVE YOU GRANDMA!")
+    if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
     end
 end
